b6a92f58d1958fbf07c64e75ed95f3e0f9727bb1
Gia Bao <baolgps38788@gmail.com>
2025-07-02 10:27:05 +0700

    feat: dreams new

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,22 +11,24 @@
 // };
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+   darkMode: 'class',
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}", // ← quan trọng với folder src
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      animation: {
-        fadeIn: "fadeIn 0.4s ease-out",
-      },
-      keyframes: {
-        fadeIn: {
-          "0%": { opacity: "0", transform: "scale(0.95)" },
-          "100%": { opacity: "1", transform: "scale(1)" },
-        },
+ theme: {
+  extend: {
+    animation: {
+      'fade-in-up': 'fadeInUp 0.4s ease-out',
+    },
+    keyframes: {
+      fadeInUp: {
+        '0%': { opacity: 0, transform: 'translateY(20px)' },
+        '100%': { opacity: 1, transform: 'translateY(0)' },
       },
     },
   },
+}
+,
   plugins: [],
 };

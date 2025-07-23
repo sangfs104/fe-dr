a8cc8242781cbaa8ea35a18a134217337bffb0a5
sangisthebest <sanggamer1042005@gmail.com>
2025-07-23 14:15:10 +0700

    fix loi

--- src/app/components/ui/ProductList1.tsx
+++ src/app/components/ui/ProductList1.tsx
@@ -1,23 +1,9 @@
 import ProductCard from "./ProductCard";
-import type { Product } from "../../types/Product"; // Đường dẫn có thể cần chỉnh lại
-
-interface Product {
-  id: number;
-  name: string;
-  description: string;
-  status: string;
-  img: any[];
-  variant: any[];
-  category: {
-    id: number;
-    name: string;
-  };
-}
+import type { Product } from "../../types/Product"; 
 
 export default function ProductList({ products }: { products: Product[] }) {
   if (!products || !Array.isArray(products)) return null;
   return (
-    // <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
       {products.map((product) => (
         <ProductCard key={product.id} product={product} />

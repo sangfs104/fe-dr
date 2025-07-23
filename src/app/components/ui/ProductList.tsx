ea37f084c437c66141aad9dee7f9da4412d6329f
sangisthebest <sanggamer1042005@gmail.com>
2025-07-23 14:07:11 +0700

    fix loi

--- src/app/components/ui/ProductList.tsx
+++ src/app/components/ui/ProductList.tsx
@@ -628,9 +628,10 @@ export default function ProductCard({ product }: { product: Product }) {
   const [mainImage, setMainImage] = useState<string | undefined>(
     defaultImg?.name
   );
-  const [selectedVariant, setSelectedVariant] = useState<
-    ProductVariant | undefined
-  >(product.variant?.[0]);
+  // Đúng (nếu không dùng setSelectedVariant)
+  const [selectedVariant] = useState<ProductVariant | undefined>(
+    product.variant?.[0]
+  );
   const [showModal, setShowModal] = useState(false);
 
   const handleImageHover = (imgName: string) => {

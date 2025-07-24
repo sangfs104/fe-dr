687cd487ee2ca4c189f34b7ed391ab6e9bfe4a7e
sangisthebest <sanggamer1042005@gmail.com>
2025-07-24 15:45:45 +0700

    fix loi

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "http",
+//         hostname: "127.0.0.1",
+//         port: "8000",
+//         pathname: "/storage/**",
+//       },
+//     ],
+//   },
+// };
+
+// module.exports = nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     remotePatterns: [
       {

31948396199559c0dceb5633ef51f3253242391f
sangisthebest <sanggamer1042005@gmail.com>
2025-07-24 14:45:50 +0700

    fix loi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,18 +73,18 @@
 //     </>
 //   );
 // }
-import BannerCarousel from "../components/ui/BannerCarousel";
-import HeaderHome from "../components/ui/Header";
-import ProductTabs from "../components/ui/ProductTabs";
-import ServiceHighlights from "../components/ui/ServiceHighlights";
-import HeroSlider from "../components/ui/HeroSlider";
-import Footer from "../components/ui/Footer";
-import FlashSaleList from "../components/ui/FlashSaleList";
-import CategoryProduct from "../components/ui/CategoryProduct";
-import ShopArticle from "../components/ui/ShopArticle";
-import VoiceQuickOrderTest from "../components/ui/VoiceQuickOrderTest";
-import AIRecommendedProducts from "../components/ui/AIRecommendedProducts";
-import PostList from "../components/post/PostList";
+import BannerCarousel from "./components/ui/BannerCarousel";
+import HeaderHome from "./components/ui/Header";
+import ProductTabs from "./components/ui/ProductTabs";
+import ServiceHighlights from "./components/ui/ServiceHighlights";
+import HeroSlider from "./components/ui/HeroSlider";
+import Footer from "./components/ui/Footer";
+import FlashSaleList from "./components/ui/FlashSaleList";
+import CategoryProduct from "./components/ui/CategoryProduct";
+import ShopArticle from "./components/ui/ShopArticle";
+import VoiceQuickOrderTest from "./components/ui/VoiceQuickOrderTest";
+import AIRecommendedProducts from "./components/ui/AIRecommendedProducts";
+import PostList from "./components/post/PostList";
 
 interface ProductPageProps {
   searchParams?: { [key: string]: string | string[] | undefined };

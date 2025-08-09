5c9452c3b0eacf1d22d79799e5f4ee79aca9c151
sangisthebest <sanggamer1042005@gmail.com>
2025-08-09 11:27:47 +0700

    fix loi domain

--- next.config.ts
+++ next.config.ts
@@ -1,70 +1,24 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "http",
-//         hostname: "127.0.0.1",
-//         port: "8000",
-//         pathname: "/img/**",
-//       },
-//       {
-//         protocol: "http",
-//         hostname: "127.0.0.1",
-//         port: "8000",
-//         pathname: "/storage/**",
-//       },
-//       {
-//         protocol: "http",
-//         hostname: "localhost",
-//         port: "8000",
-//         pathname: "/img/**",
-//       },
-//       {
-//         protocol: "http",
-//         hostname: "localhost",
-//         port: "8000",
-//         pathname: "/storage/**",
-//       },
-//       {
-//         protocol: "http",
-//         hostname: "127.0.0.1",
-//         port: "8000",
-//         pathname: "/**", 
-//       },
-//       {
-//         protocol: "http",
-//         hostname: "localhost",
-//         port: "8000",
-//         pathname: "/**", 
-//       }
-//     ],
-//   },
-// };
-
-// module.exports = nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: new URL(process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000").hostname,
-        port: new URL(process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000").port,
+        protocol: "https",
+        hostname: "dreams-admin.io.vn",
+        port: "",
         pathname: "/img/**",
       },
       {
-        protocol: "http",
-        hostname: new URL(process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000").hostname,
-        port: new URL(process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000").port,
+        protocol: "https",
+        hostname: "dreams-admin.io.vn",
+        port: "",
         pathname: "/storage/**",
       },
       {
-        protocol: "http",
-        hostname: new URL(process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000").hostname,
-        port: new URL(process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000").port,
+        protocol: "https",
+        hostname: "dreams-admin.io.vn",
+        port: "",
         pathname: "/**",
       },
     ],

a1c8527484faf03e8364e0ad95ee9eab23b19e7e
sangisthebest <sanggamer1042005@gmail.com>
2025-08-12 14:23:27 +0700

    about

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import FashionIntro3D from "../components/ui/About";
+
+export default function PostsPage() {
+  return (
+    <>
+      <main>
+        <FashionIntro3D></FashionIntro3D>
+      </main>
+    </>
+  );
+}
